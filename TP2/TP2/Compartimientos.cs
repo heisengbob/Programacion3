2ce16d774f826bb9dbe917a4d72f9f654c191d9a
heisengbob <48842967+heisengbob@users.noreply.github.com>
2019-04-09 01:24:31 -0300

    TP N2

---
+++ TP2/TP2/Compartimientos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    public class Compartimientos
+    {
+        int id;
+        int idProducto;
+        bool Estado;
+    }
+}
